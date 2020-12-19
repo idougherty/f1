0bc0eaa4835195cc374bd493cdf41f9864c12336
xavi0111 <xcrawford@hawk.iit.edu>
2020-12-19 17:31:20 -0600

    completed the first page in html ready for it to get pretty

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let canvas = document.getElementById("canvas");
+let canvas = document.getElementById("news");
 let c = canvas.getContext("2d");
 
 c.fillStyle = "black";
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 c.fillStyle = "white";
-c.fillText("test", 100, 200);
+c.fillText("pp haha", 100, 200);
