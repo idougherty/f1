1b0538ba121567ad27bc8a4265212842d172a8cc
pizza rolls <thegrandllama@gmail.com>
2020-12-23 19:07:45 -0600

    organized files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("news");
+let c = canvas.getContext("2d");
+
+c.fillStyle = "black";
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+c.fillStyle = "white";
+c.fillText("pp haha", 100, 200);
