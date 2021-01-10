0df1f16930d1fd1737d351b4fa06ccc50aa86688
xavi0111 <xcrawford@hawk.iit.edu>
2021-01-09 21:10:52 -0600

    the car done! rough structure of drivers

--- src/js/car/car.js
+++ src/js/car/car.js
@@ -1,9 +1,6 @@
 /*
 weight
-aero
-rev
 size
-fuel efficiency
 traction
 handling
 acceleration
@@ -11,14 +8,69 @@ top speed
 deceleration
 */
 
+
 class car {
 	constructor(fr, s, b, e, fu, a, t) {
-		frame = fr;
-		suspension = s;
-		brakes = b;
-		engine = e;
-		fuel = fu;
-		aero = a;
-		tires = t; //array of 4
+		this.frame = fr;
+		this.sus = s;
+		this.brake = b;
+		this.eng = e;
+		this.fuel = fu;
+		this.aero = a;
+		this.tires = t; //array of four tires
+		this.parts = [this.frame, sus, brake, eng, this.fuel, this.aero, tires[0], tires[1], tires[2], tires[3]];
 	}
+
+	weight(){
+		let ret = 0;
+		for(let i = 0; i < this.parts.length; i++){
+			ret += this.parts[i].curWeight;
+		}
+		return ret;
+	}
+
+	size(){
+		return (this.frame.size + this.aero.size);
+	}
+	
+	avgTQ(){
+		return (tires[0].quality + tires[1].quality + tires[2].quality + tires[3].quality)/4;
+	}
+
+	
+	traction(){
+		return (this.avgTQ*(this.weight+this.aero.downF));
+	}
+
+	handle(){
+		return (this.traction * this.sus.spronginess);
+	}
+
+	acceleration(){
+		//fuel quality, revs, hrps, traction, drag, weight
+		return((this.fuel.quality*this.eng.hrp)+(this.eng.maxRev+this.traction))/(this.aero.drag+this.weight);
+	}
+
+	topSpeed(){
+		//weight, drag, hrps, revs, traction, fuel quality
+		return (this.eng.maxRev*this.acceleration)/this.aero.drag;
+	}
+
+	deceleration(){
+		//traction, breaks, weight, drag 
+		return (this.traction * this.brake.quality + this.aero.drag)/this.weight
+	}
+
+	/*
+	straight
+	turn 
+	*/
+
+	idealSpeed(radius, angle){
+		return Math.sqrt(radius * angle * this.traction);
+	}
+
+
+
+
 }
